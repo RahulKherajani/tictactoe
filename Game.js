f0fa826bf840dd8fc0b39a5816739afc24c41864
RahulKherajani <rahulkherajani20@gmail.com>
2020-05-27 13:04:56 +0530

    Added Files
    Added Game Files

---
+++ Game.js
@@ -0,0 +1,37 @@
+// Restart Game Button
+var restart = document.querySelector('#b');
+
+// Grab all the squares
+var squares = document.querySelectorAll("td");
+
+
+// Clear Squares Function
+function clearBoard() {
+  for (var i = 0; i < squares.length; i++) {
+      squares[i].textContent = '';
+  }
+
+}
+restart.addEventListener('click',clearBoard)
+
+
+
+
+// Create a function that will check the square marker
+function changeMarker(){
+    if(this.textContent === ''){
+      this.textContent = 'X';
+      this.style.color='red';
+      // console.log(marker)
+    }else if (this.textContent === 'X') {
+      this.textContent = 'O';
+      this.style.color='blue';
+    }else {
+      this.textContent = '';
+    }
+};
+
+// Use a for loop to add Event listeners to all the squares
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', changeMarker);
+}
